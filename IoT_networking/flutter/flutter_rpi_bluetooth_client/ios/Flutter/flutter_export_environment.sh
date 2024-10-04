2920aaeb419edc6621c6f059cf24c325c75f0bd6
ghosananda <ghosanandaw@gmail.com>
2024-10-04 00:59:39 -0600

    adding flutter bluetooth reference code

---
+++ IoT_networking/flutter/flutter_rpi_bluetooth_client/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\ghosa\fvm\versions\3.7.12"
+export "FLUTTER_APPLICATION_PATH=D:\Documents\Private\Masters\UIUC Master\CS437_internet_of_things_Fall_2024\CS437_github\CS437\IoT_networking\flutter\flutter_rpi_bluetooth_client"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
