9423a9a75389cf9b7efaa965e259bd0d9bec3ff0
ghosanandaw <ghosanandaw@gmail.com>
2024-10-06 20:27:00 -0600

    added logic for car control

--- IoT_networking/electron/fecontrol/wifi_server.py
+++ IoT_networking/electron/fecontrol/wifi_server.py
@@ -1,25 +1,33 @@
 import socket
 import gpiozero
 import json
+from picarx import Picarx
 
 HOST = "192.168.1.9" # IP address of your Raspberry PI
 PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
 socket.socket(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-#telemetry
+
+#PiCarX init and telemetry
+px = Picarx()
 CPU_temp:float = gpiozero.CPUTemperature().temperature;
 print (CPU_temp)
+ultrasonic_distance = round(px.ultrasonic.read(), 2);
+power=10
 
 #data object to send
 class dataObject:
     CPU_temp:float=0;
+    ultrasonic_distance:float=0;
 
-    def __init__(self, CPU_temp):
+    def __init__(self, CPU_temp,ultrasonic_distance):
         self.CPU_temp = CPU_temp
+        self.ultrasonic_distance=ultrasonic_distance
     
-    def JSON_format(self,CPU_temp):
+    def JSON_format(self,CPU_temp, ultrasonic_distance):
         self.CPU_temp=CPU_temp
-        return {"CPU_temp":self.CPU_temp}
+        self.ultrasonic_distance=ultrasonic_distance
+        return {"CPU_temp":self.CPU_temp, "ultrasonic_distance":self.ultrasonic_distance}
 
 
 
@@ -28,31 +36,50 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     active_binding_status:bool=True;
     CPU_temp=gpiozero.CPUTemperature().temperature;
-    dataObjectToSend= dataObject(CPU_temp)
+    ultrasonic_distance=round(px.ultrasonic.read(), 2)
+    dataObjectToSend= dataObject(CPU_temp, ultrasonic_distance)
     
     try:
         while active_binding_status==True:
             client, clientInfo = s.accept()
             print("server recv from: ", clientInfo)
 
-            dataObjectToSend.JSON_format(gpiozero.CPUTemperature().temperature)
-            print (dataObjectToSend.JSON_format(gpiozero.CPUTemperature().temperature))
+            dataObjectConstruction=dataObjectToSend.JSON_format(gpiozero.CPUTemperature().temperature, round(px.ultrasonic.read(), 2))
+            # print (dataObjectToSend.JSON_format(gpiozero.CPUTemperature().temperature))
 
             data = client.recv(1024)      # receive 1024 Bytes of message in binary format
-            # if data != b"":
-            #     print(data)
-            #     client.sendall(data) # Echo back to client
-            print (json.dumps(dataObjectToSend.JSON_format(gpiozero.CPUTemperature().temperature)))
+            if data != b"":
+                # print(data)
+                # client.sendall(data) # Echo back to client
+            # print (json.dumps(dataObjectToSend.JSON_format(gpiozero.CPUTemperature().temperature)))
             # encoded_data_for_sendall=json.dumps(dataObjectToSend.JSON_format(gpiozero.CPUTemperature().temperature)).encode()
-            encoded_data_for_sendall=json.dumps(dataObjectToSend.JSON_format(gpiozero.CPUTemperature().temperature)).encode()
-            client.sendall(encoded_data_for_sendall)
+                encoded_data_for_sendall=json.dumps(dataObjectConstruction).encode()
+                client.sendall(encoded_data_for_sendall)
+                if data ==b"87":
+                    px.set_dir_servo_angle(0)
+                    px.forward(power);
+                elif data ==b"82":
+                    px.set_dir_servo_angle(0)
+                    px.backward(power);
+                elif data ==b"68":
+                    px.set_dir_servo_angle(30)
+                    px.forward(power);
+                elif data ==b"65":
+                    px.set_dir_servo_angle(-30)
+                    px.forward(power);
+                else:
+                    px.set_dir_servo_angle(0);
+                    px.forward(0);
+                    px.backward(0);
 
-            if data==b"break\r\n":
+
+
+            if data==b"break":
                 active_binding_status=False;
     
     except: 
         print("Closing socket")
-        client.sendall('closing socket')
+        client.sendall(b'closing socket')
         client.close()
         s.close()
 
