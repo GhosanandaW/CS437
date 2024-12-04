032ebb3085c972d5896ae7294ff7cd0b759ea148
ghosananda <ghosanandaw@gmail.com>
2024-12-04 17:27:13 +0700

    added 2 additional simulation data

--- wildlife_conservation/wildlife_conservation_integrated.ino
+++ wildlife_conservation/wildlife_conservation_integrated.ino
@@ -1,13 +1,17 @@
+//Pulse oximeter
 #include "PulseOximeter.h"
 #define PULSE_OXI_PIN 2
 PulseOximeter pox;
 
+//GPS
 #include "Adafruit_GPS.h"
 Adafruit_GPS GPS;
 
+//air quality sensor
 #include "MQ135.h"
 MQ135 gasSensor = MQ135(A0);
 
+//humidity and temperature sensor
 #include "DHT.h"
 #define DHTPIN 7
 DHT dht(DHTPIN);
@@ -15,6 +19,7 @@ DHT dht(DHTPIN);
 void setup() {
     pinMode(PULSE_OXI_PIN, INPUT);
     pinMode(1, OUTPUT);
+    pinMode(8, INPUT);
     Serial.begin(9600);
     dht.begin();
 }
@@ -23,6 +28,7 @@ void loop() {
     bool isInCelcius = true;
 
     if (Serial.available() > 0){
+        Serial.println("");
         //pulseoximeter section
         if (pox.begin()) {
             string hr=pox.getHeartRate();
@@ -41,7 +47,15 @@ void loop() {
         Serial.print("PPM is: ");
         Serial.println(ppm);
 
-        //humidity and temperature section
+        int digitalResult = digitalRead(8);
+        if (digitalResult == HIGH) {
+            Serial.println("Gas DETECTED!");
+            }
+        else {
+            Serial.println("Gas not detected");
+        }
+
+        //humidity and temperature sensor section
         float h = dht.readHumidity() * 100; // Convert to %
         Serial.println("Humidity: " + to_string(h) + "%; ");
         float t = dht.readTemperature(isInCelcius);   
@@ -55,11 +69,11 @@ void loop() {
             string mystr = Serial.readMessage();
             Serial.println("Printed by receiver:" + mystr);
         }
-        
+
         //transmit gossip information
         string msg = "Hello from a Zebra!";
+        Serial.sendMessage(1, msg);
         Serial.println("");
-        Serial.sendMessage(0, msg);
 
 
         //end of sensor reading
@@ -67,5 +81,5 @@ void loop() {
     } 
     else {
     }
-    delay(2000);
+    delay(1000);
 }
