42ae4272cacd35ce366d6c4293651511017be528
ghosanandaw <ghosanandaw@gmail.com>
2024-12-13 00:27:30 +0700

    added picture preview and directory naming feature

--- caim_project/caim/src/app/facelibrary/page.tsx
+++ caim_project/caim/src/app/facelibrary/page.tsx
@@ -13,6 +13,8 @@ async function uploadImageToAzure(imageFile: any) {
 
   // Create a DefaultAzureCredential object
   // const credential = new DefaultAzureCredential();
+
+  const pictureName=document.getElementById("pictureName").value;
   const credential = new InteractiveBrowserCredential({
     tenantId: "",
     clientId: ""
@@ -33,7 +35,7 @@ async function uploadImageToAzure(imageFile: any) {
   const blobName = `image-${Date.now()}.jpg`;
 
   // Get a reference to the container
-  const containerClient = blobServiceClient.getContainerClient('captures/knownFaces');
+  const containerClient = blobServiceClient.getContainerClient('captures/knownFaces/'+pictureName);
 
   // Create a block blob client
   const blockBlobClient = containerClient.getBlockBlobClient(blobName);
@@ -62,12 +64,12 @@ async function downloadAllFromStorage() {
   const containerClient = blobServiceClient.getContainerClient('captures');
   let i = 1;
 
-  for await (const blob of containerClient.listBlobsFlat()){
+  for await (const blob of containerClient.listBlobsFlat()) {
     console.log(`Blob ${i++}: ${blob.name}`);
-    if (blob.name.startsWith('knownFaces/')){
+    if (blob.name.startsWith('knownFaces/') && blob.name.endsWith(".jpg")) {
       const blobClient = containerClient.getBlobClient(blob.name)
       const downloadBlockBlobResponse = await blobClient.download();
-      if (downloadBlockBlobResponse) { 
+      if (downloadBlockBlobResponse) {
         const blobToDownload = await downloadBlockBlobResponse.blobBody;
         const downloadableUrl = URL.createObjectURL(blobToDownload);
         // Download using a hidden anchor tag (more browser-compatible)
@@ -144,6 +146,12 @@ async function blobToString(blob: Blob): Promise<string> {
   });
 }
 
+function showPicture() {
+  return (
+    <div><img height={400} width={600} src='https://www.idahostatesman.com/latest-news/w4ojx5/picture277864073/alternates/FREE_1140/0801%2006%20state%20street%20crashes.jpg' ></img></div>
+  )
+}
+
 
 const facelibrary = () => {
   // const handleUpload = () => {
@@ -165,15 +173,43 @@ const facelibrary = () => {
   return (
     <div className="container-fluid p-3">
       <label htmlFor="file" className='mb-2'>Upload picture here!</label>
-      <div className="row">
+      <img id="picture" src="#" alt="picture" style={{ display: "none" }} onError={() => {
+        const img = document.getElementById('picture');
+        if (img.complete && img.naturalWidth > 0) {
+          img.style.display = 'block';
+        } else {
+          // Image failed to load
+          img.style.display = 'none';
+        }
+      }} />
+      <div className="row mt-3">
         <div className="col">
-          <input id="file" name="file" type="file" />
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          <input id="file" name="file" type="file" onChange={() => {
+            const fileInput = document.getElementById('file') as HTMLInputElement;
+            const selectedFile = fileInput.files![0];
+            const reader = new FileReader();
+            reader.onload = (e) => {
+              document.getElementById('picture').src = reader.result as string;
+            };
+            reader.readAsDataURL(selectedFile);
+            const img = document.getElementById('picture');
+            img.style.display = 'block'
+          }} />
+        </div>
+      </div>
+      <div className='row mt-3'>
+        <div className="col">
+          <input id='pictureName' type='text' className="bg-transparent placeholder:text-green-400 text-green-700 text-sm border border-green-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-green-500 hover:border-green-300 shadow-sm focus:shadow"/>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ms-3"
             onClick={() => {
               const fileInput = document.getElementById('file') as HTMLInputElement;
               const selectedFile = fileInput.files![0]; // Assuming there's always one file selected
               uploadImageToAzure(selectedFile);
             }}>Upload</button>
+        </div>
+      </div>
+      <div className='row mt-3'>
+        <div className="col">
           <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
             onClick={() => { downloadAllFromStorage() }}>Download</button>
         </div>
